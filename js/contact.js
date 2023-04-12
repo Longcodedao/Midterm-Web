e4792bbbc19868e97f085159e5adce511ad85636
Iantaif <95291823+Iantaif@users.noreply.github.com>
2023-04-12 19:39:55 +0700

    contact edit
    d

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+  })
+  
+  sr.reveal('.Contactinfo',{delay:200,origin:'bottom'});
+  sr.reveal('.ContactInput',{delay:200,origin:'bottom'});
+  
+  
