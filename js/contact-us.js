152e2ef0a4bfc71bfab2081432928d8b729ee799
ngoinhaoto <dinhchinhle487@gmail.com>
2023-04-14 23:04:16 +0700

    uio

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,29 +1,5 @@
 // Google Map Api
-let map;
-
-async function initMap() {
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  let ourPlace = { lat: 16.07109753993075, lng: 108.22024833862913 };
-
-  map = new Map(document.getElementById("map"), {
-    center: { lat: 16.071044494159715, lng: 108.22024246791646 },
-    zoom: 10,
-    center: ourPlace,
-  });
-
-  const locationButton = document.createElement("button");
-  locationButton.textContent = "Current Location";
-  locationButton.classList.add("custom-map-control-button");
-
-  let marker = new google.maps.Marker({
-    position: ourPlace,
-    map: map,
-    title: "OUR PLACE, HALLELUJAH",
-  });
-}
-
-initMap();
+// Initialize and add the map
 
 // Modal PopUp
 const openModalButtons = document.querySelectorAll("[data-modal-target]");
