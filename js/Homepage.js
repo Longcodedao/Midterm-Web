028718773ed87adffbda30cd6386fd4a0ce8590e
Long Vo <116516246+Longcodedao@users.noreply.github.com>
2023-04-14 22:25:26 +0700

    Update Again

--- js/Homepage.js
+++ js/Homepage.js
@@ -19,25 +19,30 @@ var logo = document.getElementById("logo");
 var navigate_options = document.getElementById("nav-option");
 var sticky = NavBar.offsetTop;
 
-$(window).scroll(function () {
-  if ($(window).scrollTop() > sticky + 100) {
-    NavBar.classList.add("sticky");
-    logo.classList.add("white-background");
-    navigate_options.classList.add("white-background");
-  } else {
-    NavBar.classList.remove("sticky");
-    logo.classList.remove("white-background");
-    navigate_options.classList.remove("white-background");
-  }
-});
+if ($(window).width() > 960){
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > sticky + 100) {
+      NavBar.classList.add("sticky");
+      logo.classList.add("white-background");
+      navigate_options.classList.add("white-background");
+    } else {
+      NavBar.classList.remove("sticky");
+      logo.classList.remove("white-background");
+      navigate_options.classList.remove("white-background");
+    }
+  });
+} 
+
 
 $listNav = $("li.nav-opt");
 
-const screenWidth = $(window).width();
-console.log(screenWidth);
-
 $("#hamburger-icon").on("click", function () {
-  $listNav.toggle(200);
+  var scrollPosition = $(window).scrollTop()
+  localStorage.setItem('scrollPosition', scrollPosition) 
+  $listNav.slideToggle(200);
+  var storedScrollPosition = localStorage.getItem('scrollPosition');
+  $(window).scrollTop(storedScrollPosition);
+  console.log($(window).scrollTop())
 });
 
 if ($("window").width() > 600) {
