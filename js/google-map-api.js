ea3d635a9128242bdfec4d83f41d769d0536d59c
ngoinhaoto <dinhchinhle487@gmail.com>
2023-04-15 12:33:03 +0700

    update googleapi

--- js/google-map-api.js
+++ js/google-map-api.js
@@ -2,27 +2,66 @@ let map;
 
 async function Initialize() {
   // The location of our business(its VNUK because i am homeless)
-
-  const position = { lat: 16.071137280035177, lng: 108.2202210102441 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 16.07115789911272, lng: 108.22023173908029 },
+    zoom: 15,
+  });
+  const request = {
+    placeId: "ChIJvcaESTEYQjERy7hqJD8EOrU",
+    fields: ["name", "formatted_address", "geometry"],
+  };
   const infowindow = new google.maps.InfoWindow();
   const service = new google.maps.places.PlacesService(map);
 
-  // The map, centered at VNUK
-  map = new Map(document.getElementById("map"), {
-    zoom: 12,
-    center: position,
-  });
+  service.getDetails(request, (place, status) => {
+    if (
+      status === google.maps.places.PlacesServiceStatus.OK &&
+      place &&
+      place.geometry &&
+      place.geometry.location
+    ) {
+      const marker = new google.maps.Marker({
+        map,
+        position: place.geometry.location,
+        title: "VNUK or something ",
+      });
 
-  // The marker, positioned at VNUK
+      google.maps.event.addListener(marker, "click", () => {
+        const content = document.createElement("div");
+        const nameElement = document.createElement("h2");
 
-  const marker = new google.maps.Marker({
-    map: map,
-    position: position,
-    title: "VNUK or something idk",
+        nameElement.textContent = place.name;
+        nameElement.style.fontSize = "23px";
+        nameElement.style.color = "green";
+        nameElement.style.fontFamily = "Be Vietnam Pro";
+
+        content.appendChild(nameElement);
+
+        const placeIdElement = document.createElement("p");
+
+        const placeDescription = document.createElement("p");
+        placeDescription.textContent = "It's VNUK because I am homeless";
+        placeDescription.style.color = "green";
+        placeDescription.style.fontSize = "16px";
+        placeDescription.style.fontFamily = "Be Vietnam Pro";
+        placeDescription.style.marginTop = "6px";
+        placeDescription.style.marginBottom = "4px";
+
+        placeIdElement.textContent = place.place_id;
+        content.appendChild(placeIdElement);
+        content.appendChild(placeDescription);
+
+        const placeAddressElement = document.createElement("p");
+
+        placeAddressElement.textContent = place.formatted_address;
+        placeAddressElement.style.fontSize = "16px";
+        placeAddressElement.style.fontFamily = "Be Vietnam Pro";
+
+        content.appendChild(placeAddressElement);
+        infowindow.setContent(content);
+        infowindow.open(map, marker);
+      });
+    }
   });
 }
 
