08bc726eb3cc375bd34e2985c99fced2108e23f6
ngoinhaoto <dinhchinhle487@gmail.com>
2023-06-11 19:31:37 +0700

    update

--- js/product-listing.js
+++ js/product-listing.js
@@ -33,3 +33,44 @@ $("#hamburger-icon").on("click", function () {
 if ($("window").width() > 600) {
   $listNav.show();
 }
+
+// -------------GENERATE RANDOM FUNKY COLOR FOR THE BUYING BUTTON------------------
+var colors = [
+  "#2c3e50",
+  "#d42b6a",
+  "#43bc8d",
+  "#60b748",
+  "#2aced5",
+  "#3c81c3",
+  "#f39c12",
+  "#4b3bc4",
+  "#7822dd",
+  "#b21ce3",
+  "#ca35b6",
+  "#c1673e",
+  "#62d12e",
+  "#4a80b5",
+  "#c44e3b",
+  "#d5882a",
+  "#d82767",
+  "#fd79a8",
+  "#8adb24",
+  "#2327dc",
+  "#21de96",
+  "#c8d32c",
+  ,
+];
+
+let buyingButtons = $(".buying-button");
+let price = $(".price");
+// using vanilla js cuz its the only way to do this
+for (let i = 0; i < buyingButtons.length; i++) {
+  let randomColor = colors[Math.floor(Math.random() * colors.length)];
+  price[i].style.color = randomColor;
+  buyingButtons[i].style.backgroundColor = randomColor;
+}
+// --------------------- JQUERYUI ANIMATION ----------------------
+
+$(document).ready(function () {
+  console.log("Jqueryloaded");
+});
