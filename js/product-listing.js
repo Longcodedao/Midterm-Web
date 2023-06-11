43373f119b19955c3e21e32ef8fc58c2d8590965
ngoinhaoto <dinhchinhle487@gmail.com>
2023-06-11 19:47:01 +0700

    update

--- js/product-listing.js
+++ js/product-listing.js
@@ -35,6 +35,7 @@ if ($("window").width() > 600) {
 }
 
 // -------------GENERATE RANDOM FUNKY COLOR FOR THE BUYING BUTTON and price------------------
+
 var colors = [
   "#2c3e50",
   "#d42b6a",
@@ -49,7 +50,6 @@ var colors = [
   "#ca35b6",
   "#c1673e",
   "#62d12e",
-  "#4a80b5",
   "#c44e3b",
   "#d5882a",
   "#fd79a8",
@@ -57,6 +57,15 @@ var colors = [
   "#2327dc",
   "#21de96",
   "#c8d32c",
+  "#e4af1d",
+  "#631de4",
+  "#e45c1d",
+  "#684b96",
+  "#4c4ec9",
+  "#3bbb94",
+  "#17ac1e",
+  "#9dbb16",
+  "#cac036",
 ];
 
 let buyingButtons = $(".buying-button");
@@ -77,8 +86,5 @@ for (let i = 0; i < buyingButtons.length; i++) {
   $(prices[i]).css("color", randomColor);
   $(buyingButtons[i]).css("background-color", randomColor);
 }
-// --------------------- JQUERYUI ANIMATION ----------------------
 
-$(document).ready(function () {
-  console.log("Jqueryloaded");
-});
+// --------------------- JQUERYUI ANIMATION ----------------------
