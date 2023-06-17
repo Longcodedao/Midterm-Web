829539de353c263f91b31803749d5ce42de9ef68
ngoinhaoto <dinhchinhle487@gmail.com>
2023-06-17 12:01:29 +0700

    update scroll to top and down button using jquery animate

--- js/AboutUs.js
+++ js/AboutUs.js
@@ -119,3 +119,44 @@ function toggleAnimationClass() {
 
 // add an event listener that calls the toggleAnimationClass function on scroll
 window.addEventListener("scroll", toggleAnimationClass);
+
+// --------------- JQUERY ANIMATE top to bottom scroll ------------------------------
+$(document).ready(function () {
+  // Scroll to bottom button
+  $("#scrollToBottomBtn").click(function (event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+
+    return false;
+  });
+
+  // Scroll to top button
+  $("#scrollToTopBtn").click(function (event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    $("html").animate({ scrollTop: 0 }, "slow"); // Added this line
+    return false;
+  });
+
+  // Show/hide the buttons based on scroll position
+  $(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+    var windowHeight = $(this).height();
+    var documentHeight = $(document).height();
+
+    if (scrollTop > windowHeight / 2) {
+      $("#scrollToTopBtn").addClass("active");
+    } else {
+      $("#scrollToTopBtn").removeClass("active");
+    }
+
+    if (scrollTop + windowHeight < documentHeight - windowHeight / 2) {
+      $("#scrollToBottomBtn").addClass("active");
+    } else {
+      $("#scrollToBottomBtn").removeClass("active");
+    }
+  });
+
+  // Check initial scroll position to show/hide the buttons
+  $(window).trigger("scroll");
+});
